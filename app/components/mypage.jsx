3e12787fb1577a0493878d4e55cb29bbf4f606d1
Suzan Tadha <suzzantadha@gmail.com>
2026-03-02 15:16:04 -0600

    created initial component

---
+++ app/components/mypage.jsx
@@ -0,0 +1,11 @@
+export default function Mypage() {
+    return (
+        <><div>
+            <header><b>Welcome To My Website!</b></header>
+            <p>My name is Suzan Tadha and I am a Computer Science student at UIC. <br></br>
+            I have experience with Java, Javascript, Python, HTML-CSS, C++. <br></br>
+            I am excited to expand my knowledge in these areas.</p>
+        </div><img src="/chicago.png" alt="Chicago From Top" width={250}/></>
+    );
+}
+ 
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import Link from "next/link";
-
+import Mypage from "./components/mypage";
 export default function Home() {
   return (
+    <div>
     <div className="flex flex-row gap-4">
       <Link href="/about">About</Link>
       <Link href="/contact">Contact</Link>
       <Link href="/projects">Projects</Link>
+    </div>
+    <Mypage />
     </div>
   );
 }
